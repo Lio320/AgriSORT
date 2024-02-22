f40e8f7245d3545e466233d297da539b01a0a2ba
Leonardo <saracenileonardo01@gmail.com>
2024-02-22 11:08:22 +0100

    Deleted data

--- tools/visualizer.py
+++ tools/visualizer.py
@@ -16,6 +16,6 @@ class Visualizer():
 
     def draw_track(self, track, image):
         bbox = meas_to_bbox(track.get_state())
-        cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), track.color, 2)
-        cv2.putText(image, str(track.id), (int(bbox[0]), int(bbox[3])), cv2.FONT_HERSHEY_COMPLEX, 2, track.color, 2)
+        cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), track.color, 4)
+        cv2.putText(image, str(track.id), (int(bbox[0] + 15), int(bbox[3] - 20)), cv2.FONT_HERSHEY_COMPLEX, 2, track.color, 4)
         return image
